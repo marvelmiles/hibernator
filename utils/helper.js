28d1746b57b1b3dcc9fb3b38d53294b213547b7f
marvelmiles <marvellousabidemi2@gmail.com>
2025-12-12 17:55:20 +0100

    added hasConflict check in add schedule fxn

--- utils/helper.js
+++ utils/helper.js
@@ -3,9 +3,9 @@ const joinArr = (arr, sep = ", ", lastSep = " and ") => {
   if (arr.length === 1) return arr[0];
   if (arr.length === 2) return `${arr[0]}${lastSep}${arr[1]}`;
 
-  const allButLast = arr.slice(0, -1).join(sep);
+  const others = arr.slice(0, -1).join(sep);
   const last = arr[arr.length - 1];
-  return `${allButLast}${lastSep}${last}`;
+  return `${others}${lastSep}${last}`;
 };
 
 module.exports = { joinArr };
